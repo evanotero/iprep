1694689965a8d1d4c1225f86fba425557e0f852d
evanotero <oteroev@bc.edu>
2016-03-02 19:31:22 -0500

    init

---
+++ 3-1-16/puddle.py
@@ -0,0 +1,22 @@
+def puddle(walls):
+    leftmax = 0
+    rightmax = 0
+    lIndex = 0
+    rIndex = len(walls)-1
+    volume = 0
+    while lIndex < rIndex:
+        # Adjust max values
+        if walls[lIndex] > leftmax:
+            leftmax = walls[lIndex]
+        if walls[rIndex] > rightmax:
+            rightmax = walls[rIndex]
+        #
+        if leftmax < rightmax:
+            volume += leftmax - walls[lIndex]
+            lIndex+=1
+        else:
+            volume += rightmax - walls[rIndex]
+            rIndex-=1
+    return volume
+
+print puddle([2,5,1,2,3,4,7,7,6])
