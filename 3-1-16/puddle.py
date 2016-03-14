e7b3e14af88d86e525e6869d1ca5e252d8ad727f
evanotero <oteroev@bc.edu>
2016-03-14 13:48:08 -0400

    syntax fix

--- 3-1-16/puddle.py
+++ 3-1-16/puddle.py
@@ -1,17 +1,17 @@
 def puddle(walls):
-    Lmax = Rmax = left = volume = 0
+    maxL = maxR = left = volume = 0
     right = len(walls)-1
     while left < right:
         # Adjust max values
-        Lmax = max(walls[left], Lmax)
-        Rmax = max(walls[right], Rmax)
+        maxL = max(walls[left], maxL)
+        maxR = max(walls[right], maxR)
         # Calculate volume
-        if Lmax < Rmax:
-            volume += Lmax - walls[left]
-            left+=1
+        if maxL < maxR:
+            volume += maxL - walls[left]
+            left += 1
         else:
-            volume += Rmax - walls[right]
-            right-=1
+            volume += maxR - walls[right]
+            right -= 1
     return volume
 
 print puddle([2,5,1,2,3,4,7,7,6])
