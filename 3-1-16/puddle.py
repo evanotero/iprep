1bb786233f84bf287cddb8fe175674645b6aa15d
evanotero <oteroev@bc.edu>
2016-03-15 22:15:45 -0400

    more test cases

--- 3-1-16/puddle.py
+++ 3-1-16/puddle.py
@@ -16,7 +16,7 @@ def puddle(walls):
 
 print puddle([2,5,1,2,3,4,7,7,6])
 
-# Test
+# Test Cases
 assert puddle([]) == 0
 assert puddle([5]) == 0
 assert puddle([5, 5]) == 0
@@ -24,8 +24,8 @@ assert puddle([5, 0, 5]) == 5
 assert puddle([2, 5, 1, 2, 3, 4, 7, 7, 6, 8]) == 11
 assert puddle([2, 5, 1, 2, 3, 4, 7, 7, 6])  == 10
 assert puddle([6, 7, 7, 4, 3, 2, 1, 5, 2])  == 10
+assert puddle([2,5,1,2,3,4,7,7,6,3,5]) == 12
 assert puddle([2, 5, 1, 3, 1, 2, 1, 7, 7, 6])  == 17
 assert puddle([2, 7, 2, 7, 4, 7, 1, 7, 3, 7])  == 18
 assert puddle([2, 5, 1, 2, 3, 4, 7, 7, 6, 2, 7, 1, 2, 3, 4, 5, 5, 4])  == 26
-
-
+assert puddle([9,6,2,7,10,2,8,2,9,10]) == 31
