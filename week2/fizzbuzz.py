430eb28344efc471958a2a802dd8163afa168bc5
evanotero <oteroev@bc.edu>
2016-03-21 09:49:16 -0400

    init

---
+++ week2/fizzbuzz.py
@@ -0,0 +1,14 @@
+# FizzBuzz
+
+def fizzbuzz(n):
+    if n % 3 == 0 and n % 5 == 0:
+        return "FizzBuzz"
+    elif n % 3 == 0:
+        return "Fizz"
+    elif n % 5 == 0:
+        return "Buzz"
+    else:
+        return num
+
+for num in range(1,101):
+    print fizzbuzz(num)
